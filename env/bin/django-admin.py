f8ed04a3f276b33ce21b9c54c82dfd84fbe32320
dzionek <vburtoch@gmail.com>
2020-02-29 14:46:47 +0000

    New models and env

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/bartek/Desktop/htb2020/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ src/users/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def login(request):
+    """ login view """
+    return HttpResponse("Here will be the login page")
+
+def register(request):
+    """ register view """
+    return HttpResponse("Here will be the register page")
+
+def user(request):
+    return HttpResponse("Here user will configure its account")
