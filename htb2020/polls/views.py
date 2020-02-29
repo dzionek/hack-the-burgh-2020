7817243f7eb67aedb7f93b76b701cf16477347fd
maximechemenda <maxime.chemenda@gmail.com>
2020-02-29 13:29:36 +0000

    changes

--- htb2020/polls/views.py
+++ htb2020/polls/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, worldd. You're at the polls index.")
+    return HttpResponse("Hello, worlddddddddddddfdflqs. You're at the polls index.")
