6e7a6154f585ffc83c43b9f11c5326d2b014e88a
dzionek <vburtoch@gmail.com>
2020-02-29 23:58:39 +0000

    Patient model

--- src/map/models.py
+++ src/map/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import json
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    has_coronavirus = models.BooleanField(default=False)
+    places_list = models.TextField(default='[]')
+
+    @property
+    def list(self):
+        return json.loads(self.places_list)
+
+    @list.setter
+    def list(self, value):
+        self.places_list = json.dumps(self.list + value)
--- src/map/views.py
+++ src/map/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 import config
 # Create your views here.
 def add_place_view(request, *args, **kwargs):
-    context = {'api_key': config.api_key}
-    return render(request, 'map/add-place.html', context)
+    if request.user.is_authenticated:
+        context = {'api_key': config.api_key}
+        return render(request, 'map/add-place.html', context)
+    else:
+        return HttpResponseRedirect('/home')
